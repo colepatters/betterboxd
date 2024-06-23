67fca94435f5ab91f80c4ded5309f8bbdd250d39
Cole Patterson <cole.patter@gmail.com>
2024-06-23 10:55:31 -0500

    clean house

--- src/lib/db/server-firestore.ts
+++
@@ -1,7 +0,0 @@
-import { getAdmin } from "$lib/firebase-server";
-
-export function getFirestore() {
-  const admin = getAdmin();
-  const firestore = admin.firestore();
-  return firestore;
-}
--- src/lib/icons/star_half.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    export let size = 24
-</script>
-
-<svg xmlns="http://www.w3.org/2000/svg" height={size} viewBox="0 -960 960 960" width={size}><path d="m606-286-33-144 111-96-146-13-58-136v312l126 77ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"/></svg>
